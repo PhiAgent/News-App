2852e6114a3d2498364439d95a31d698129702da
PhiAgent <princegyekyeaddai@gmail.com>
2022-03-15 00:33:35 -0500

    middleware types

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,25 @@
+
+//ENVIRONMENT
+import * as dotenv from 'dotenv';
+import path from 'path';
+
 // SERVER
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import path from 'path';
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT: number = parseInt(process.env.PORT as string, 10) || 8080;
+
+// MIDDLEWARE
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
-})
+});
+
+dotenv.config;
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
