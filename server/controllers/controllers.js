1f19633966f84e3f3dc1f389765daafbb92cda4a
PhiAgent <princegyekyeaddai@gmail.com>
2022-03-17 22:45:45 -0500

    Switched backend to node.js

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -2,15 +2,38 @@ export { };
 import axios from 'axios';
 import { TECH_ENDPOINT, BUSINESS_ENDPOINT, WORLD_ENDPOINT } from './../models/models';
 import {Request, Response} from 'express';
+import { fetchBusinessNews } from './../../database/db';
 
+interface News {
+  id: number;
+  category: string;
+  source: string;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedOn: string;
+};
+
+interface User {
+  id: number;
+  username: string;
+  admin: boolean;
+};
+
+interface favorite {
+  id: number;
+  userID: number;
+  newsID: number;
+};
 
 export const getBusinessNews = (req: Request, res: Response) => {
 
-  axios
-    .get(BUSINESS_ENDPOINT)
-    .then((response: any) => res.status(200).send(response.data))
-    .catch((err: any) => res.status(400).end());
+  const cb = (err: {msg: number}, news: News[]) =>
+  err ? res.status(err.msg).end : res.status(200).send(news);
 
+  fetchBusinessNews(cb);
 };
 
 export const getWorldNews = (req: Request, res: Response) => {
