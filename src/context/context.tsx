0bd3db722e6b6f10d55dc02912c4399d7c886e6a
PhiAgent <princegyekyeaddai@gmail.com>
2022-03-19 02:24:18 -0500

    settingUp Categories in Context

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -7,15 +7,24 @@ interface userInfo {
   username: string;
   userID: number;
   favorites: News[];
+  techNews: News[];
+  businessNews: News[];
+  worldNews: News[];
   setUser: (arg: string) => void;
   setUserID: (arg: number) => void;
   setFavorites: (arg: News) => void;
+  setBusinessNews: (arg: News) => void;
+  setTechNews: (arg: News) => void;
+  setWorldNews: (arg: News) => void;
 }
 
 const defaultState = {
   username: '',
   userID: 0,
   favorites: [],
+  businessNews: [],
+  techNews: [],
+  worldNews: [],
 }
 
 const ThemeContext = createContext<Partial<userInfo>>(defaultState);
@@ -26,6 +35,9 @@ export const ContextProvider: FC = ({children}) => {
   const [username, setUser] = useLocalStorage('username', defaultState.username);
   const [userID, setUserID] = useLocalStorage('userID', defaultState.userID);
   const [favorites, setFavorites] = useLocalStorage('favorites', defaultState.favorites);
+  const [businessNews, setBusinessNews] = useLocalStorage('userID', defaultState.businessNews);
+  const [techNews, setTechNews] = useLocalStorage('userID', defaultState.techNews);
+  const [worldNews, setWorldNews] = useLocalStorage('userID', defaultState.worldNews);
 
 
   return (
@@ -33,9 +45,15 @@ export const ContextProvider: FC = ({children}) => {
       username,
       userID,
       favorites,
+      techNews,
+      businessNews,
+      worldNews,
       setUser,
       setUserID,
       setFavorites,
+      setTechNews,
+      setBusinessNews,
+      setWorldNews,
     }}>
       {children}
     </ThemeContext.Provider>
---
+++ src/utils/utils.tsx
@@ -0,0 +1,5 @@
+// function that given an id
+// will search through an array
+// find an object with that id
+// delete that object
+// and return the array
